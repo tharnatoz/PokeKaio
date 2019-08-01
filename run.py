f331b5d206eae00d416545e91513fca1d1baf09e
Tharnatoz <poke.king.kai@gmail.com>
2019-08-01 15:52:15 +0200

    some fixe

--- run.py
+++ run.py
@@ -54,6 +54,6 @@ if __name__ == "__main__":
 				channel.check()
 				logger.info("%s is now waiting for %d seconds", channel.channelName, checkInterval)
 			time.sleep(checkInterval)
-	except exception as e:
+	except Exception as e:
 		print e
 
