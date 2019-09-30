845f317b2c8aa184e1b661401fffc1f014a0333d
Tharnatoz <poke.king.kai@gmail.com>
2019-09-30 20:59:03 +0200

    add pokemon name to message

--- message/messageParser.py
+++ message/messageParser.py
@@ -12,6 +12,11 @@ class MessageParser():
         # load messages.json - default
         with open('message/lang/{}/messages.json'.format(self.locale)) as f:
             self.messages = json.load(f)
+        
+        # load pokemon.json - default
+        with open('message/lang/{}/pokemon.json'.format(self.locale)) as f:
+            self.pokemon = json.load(f)
+
     
     # replace the placeholder with the infos  
     def replace(self, string, infDict):
@@ -20,6 +25,12 @@ class MessageParser():
         s = string.encode('UTF-8')
         for key in infDict:
             s = s.replace("{"+key+"}", str(infDict[key]))
+        
+        # now replace the pokemon name if a mon id is set in dict
+        if('pokemonId' in infDict):
+            pName = self.getPokemonName(infDict['pokemonId']) 
+            s = s.replace("{pokemonName}", pName)
+        
         return s
     
     # returns the message utf-8 encoded
@@ -27,3 +38,11 @@ class MessageParser():
         s = self.messages[self.messenger][key]
         string = s.encode('UTF-8')
         return string
+
+    def getPokemonName(self, id):
+        if(id > 0 and id < len(self.pokemon['name'])-1):
+            s = self.pokemon['name'][id]
+            string = s.encode('UTF-8')
+            return string
+        else:
+            return "No name found"
